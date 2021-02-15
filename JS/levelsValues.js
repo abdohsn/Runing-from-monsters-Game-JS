c3748a24bb22320d630dc596828aa01b58a991bd
sondos-medhat <56825620+sondos-medhat@users.noreply.github.com>
2021-02-16 00:05:55 +0200

    deployed

--- JS/levelsValues.js
+++ JS/levelsValues.js
@@ -14,12 +14,15 @@ var foodlvl;
 
 
 function gameOver() {
-  song = songsList[keys[2]];
-  // audioSource.src = song;
-  // audioSource.play();
+  playaudio(4);
   clearInterval(timer);
 }
 
+
+function youlose() {
+  playaudio(8);
+  clearInterval(timer);
+}
 
 
 function updateTimer() {
@@ -41,45 +44,66 @@ function start()
 {  
     timer = setInterval(updateTimer, 1000);
     updateTimer();
-
     playaudio(0);
   
 }
+
+
+var monster_level=document.getElementById("monsterlvl");
 flag=0;
 function setfoodtime(){
     if(timemin == 0){
+            monster_level.innerHTML="1";
             foodtimeappear=10000;  
             foodlvl=5;
-            document.getElementById("mainbody").classList.add('bkglvl1');
-           
+            document.getElementById("mainbody").classList.add('gameBkg1');
+            document.getElementById("gameBorder").classList.add('gameborderBkg1');
+            
 
         }
         else if(timemin == 1 ){
-          flag=1;
+          monster_level.innerHTML="2";
+            flag=1;
             foodtimeappear=8000;
             foodlvl=4;
-            document.getElementById("mainbody").classList.remove('bkglvl1');
-            document.getElementById("mainbody").classList.add('bkglvl2');
+            document.getElementById("mainbody").classList.remove('gameBkg1');
+            document.getElementById("mainbody").classList.add('gameBkg2');
 
-            
-            
+            document.getElementById("gameBorder").classList.remove('gameborderBkg1');
+            document.getElementById("gameBorder").classList.add('gameborderBkg2');  
             // if ( document.getElementById("mainbody").classList.contains('bkglvl1') )
             //     {
             //         document.getElementById("mainbody").classList.toggle('bkglvl2');
 
             //     }
         }
-        else if(timemin == 4){
+        else if(timemin == 2){
+          monster_level.innerHTML="3";
             flag=2;
             foodtimeappear=6000;
             foodlvl=3;
+
+
+            document.getElementById("mainbody").classList.remove('gameBkg2');
+            document.getElementById("mainbody").classList.add('gameBkg3');
+
+            document.getElementById("gameBorder").classList.remove('gameborderBkg2');
+            document.getElementById("gameBorder").classList.add('gameborderBkg3');
         }
-        else if(timemin == 6 ){
+        else if(timemin == 3 ){
+          monster_level.innerHTML="4";
             flag=3;
             foodtimeappear=5000;
             foodlvl=2;
+
+            document.getElementById("mainbody").classList.remove('gameBkg3');
+            document.getElementById("mainbody").classList.add('gameBkg4');
+
+            document.getElementById("gameBorder").classList.remove('gameborderBkg3');
+            document.getElementById("gameBorder").classList.add('gameborderBkg4');
         }
         else{
+          monster_level.innerHTML="5";
           flag=4;
             foodtimeappear=4000;
             foodlvl=1;
@@ -123,5 +147,5 @@ function playeventaudio(theeventnum){
   audioeventSource.play();
 }
 // to change the sound every level
-// time here change every 30 sec 1 min so i use 32000 not 60000 
+// time here change every 30 sec 1 min sos i use 32000 not 60000 
 setInterval(checktimenw, 32000);
--- index.php
+++
@@ -1,1 +0,0 @@
-<?php include_once("startplay.html"); ?>
